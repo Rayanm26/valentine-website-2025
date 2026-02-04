55ac0fec50910ff11513a70909d547265b68b957
Rayanm26 <rayanm2604@gmail.com>
2026-02-04 17:20:48 +0100

    Update music button texts to French

--- config.js
+++ config.js
@@ -79,8 +79,8 @@ const CONFIG = {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dtxobiqbf/video/upload/v1770221168/i_love_you_ztq51b.mp3", // Music streaming URL
-        startText: "🎵 Play Music",        // Button text to start music
-        stopText: "🔇 Stop Music",         // Button text to stop music
+        startText: "🎵 Joue la musique :)",        // Button text to start music
+        stopText: "🔇 Arrête la musique",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
     }
 };
